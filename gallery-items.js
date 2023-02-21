2ebdd4ae4804f9cf304c659419be5930c6612e8e
chvg1968 <conradovilla.gmail.com>
2023-02-20 19:27:00 -0500

    first commit

---
+++ gallery-items.js
@@ -0,0 +1,65 @@
+export const galleryItems = [
+  {
+    preview:
+      "/basic_images/post1.jpeg",
+    original:
+      "/modal_images/post1.jpeg",
+    description: "Jacket power",
+  },
+  {
+    preview:
+      "/basic_images/post2.jpeg",
+    original:
+      "/modal_images/post2.jpeg",
+    description: "Jacket Flower",
+  },
+  {
+    preview:
+      "/basic_images/post3.jpeg",
+    original:
+      "/modal_images/post3.jpeg",
+    description: "Hokkaido Jacket",
+  },
+  {
+    preview:
+      "/basic_images/post4.jpeg",
+    original:
+      "/modal_images/post4.jpeg",
+    description: "Paris Jacket",
+  },
+  {
+    preview:
+      "/basic_images/post5.jpeg",
+    original:
+      "/modal_images/post5.jpeg",
+    description: "New York Jacket",
+  },
+  {
+    preview:
+      "/basic_images/post6.jpeg",
+    original:
+      "/modal_images/post6.jpeg",
+    description: "Barcelona Jacket",
+  },
+  {
+    preview:
+      "/basic_images/post7.jpeg",
+    original:
+      "/modal_images/post7.jpeg",
+    description: "Tokio Jacket",
+  },
+  {
+    preview:
+      "/basic_images/post8.jpeg",
+    original:
+      "/modal_images/post8.jpeg",
+    description: "Ibiza Jacket",
+  },
+  {
+    preview:
+      "/basic_images/post9.jpeg",
+    original:
+      "/modal_images/post9.jpeg",
+    description: "London Jacket",
+  },
+];
